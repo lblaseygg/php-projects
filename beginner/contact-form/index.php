c7473b4352801e816dea678f0b8174fd1e594b3e
Luis Feliciano <justblasey@gmail.com>
2025-08-30 14:38:45 -0400

    Finished capturing form data and sanitizing input with trim and htmlspecialchars

--- beginner/contact-form/index.php
+++ beginner/contact-form/index.php
@@ -1,3 +1,37 @@
+<?php
+if (isset($_POST['name'])) {
+    $name = trim(htmlspecialchars($_POST['name']));
+} else {
+    $name = '';
+}
+
+if (isset($_POST['email'])) {
+    $email = trim(htmlspecialchars($_POST['email']));
+} else {
+    $email = '';
+}
+
+if (isset($_POST['phone'])) {
+    $phone = trim(htmlspecialchars($_POST['phone']));
+} else {
+    $phone = 'phone';
+}
+
+if (isset($_POST['message'])) {
+    $message = trim(htmlspecialchars($_POST['message']));
+} else {
+    $message = 'message';
+}
+
+var_dump($name, $email, $phone, $message);
+
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,15 +41,25 @@
 </head>
 <body>
     <p>Contact form validation using PHP</p>
-    <form>
+    <form method="post">
         <label for="name">Name:</label><br>
-        <input placeholder="Enter your name" type="text" name="name"><br><br>
+        <input placeholder="Enter your name" type="text" name="name" required><br><br>
 
         <label for="phone">Phone:</label><br>
-        <input placeholder="Enter your phone" type="phone" name="phone"><br><br>
+        <input placeholder="Enter your phone" type="phone" name="phone" required><br><br>
 
         <label for="email">Email:</label><br>
-        <input placeholder="Enter your email" type="email" name="email"><br>
+        <input placeholder="Enter your email" type="email" name="email" required><br><br>
+
+        <label for="message">Message:</label><br>
+        <textarea  
+            name="message" 
+            rows="5" 
+            cols="40" 
+            placeholder="Write your message here..."
+            required
+        ></textarea>
+        <input type="submit">
     </form>
 </body>
 </html>
