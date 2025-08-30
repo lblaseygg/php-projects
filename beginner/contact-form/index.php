2d5cbcedaa601c91309019d198b241d125a1ed9b
Luis Feliciano <justblasey@gmail.com>
2025-08-30 14:18:36 -0400

    Adding beginner folder

---
+++ beginner/contact-form/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form validation</title>
+</head>
+<body>
+    <p>Contact form validation using PHP</p>
+    <form>
+        <label for="name">Name:</label><br>
+        <input placeholder="Enter your name" type="text" name="name"><br><br>
+
+        <label for="phone">Phone:</label><br>
+        <input placeholder="Enter your phone" type="phone" name="phone"><br><br>
+
+        <label for="email">Email:</label><br>
+        <input placeholder="Enter your email" type="email" name="email"><br>
+    </form>
+</body>
+</html>
